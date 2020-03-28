bc4bf3616c60a34b05f3db5586a608ee500bc489
Omar Mujtaba <omar.mujtaba@gmail.com>
2020-03-29 03:32:26 +1100

    Add pin to map on long press

--- VirtualTourist/Controller/TravelLocationsMapViewController.swift
+++ VirtualTourist/Controller/TravelLocationsMapViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 import MapKit
 
-class TravelLocationsMapViewController: UIViewController {
+class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet private weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupLongPressListenerOnMap()
+    }
+    
+    // Setup Long Press Listener
+    private func setupLongPressListenerOnMap() {
+        let uilgr = UILongPressGestureRecognizer(target: self, action: #selector(addAnnotation(gestureRecognizer:)))
+        uilgr.minimumPressDuration = 2.0
+        mapView.addGestureRecognizer(uilgr)
+    }
+    
+    // Add Pin to map on long press
+    @objc private func addAnnotation(gestureRecognizer: UIGestureRecognizer) {
+        if gestureRecognizer.state == UIGestureRecognizer.State.began {
+            let touchPoint = gestureRecognizer.location(in: mapView)
+            let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+            mapView.addAnnotation(annotation)
+        }
     }
 
+    // Set Map Pins UI
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
 
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
 }
 
