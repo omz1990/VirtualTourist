7de45c28a58361ff0a4cd016ba74b7eebf3d419e
Omar Mujtaba <omar.mujtaba@gmail.com>
2020-03-31 20:42:15 +1100

    Compatibility for iOS 13 and below in AppDelegate

--- VirtualTourist/AppDelegate.swift
+++ VirtualTourist/AppDelegate.swift
@@ -14,9 +14,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window : UIWindow?
     
+    let dataController = DataController(modelName: "VirtualTourist")
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.rn true
+        initDataController()
         return true
+    }
+    
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        saveViewContext()
+    }
+    
+    private func initDataController() {
+        // iOS 13 and above use SceneDelegate for this
+        guard #available(iOS 13.0, *) else {
+            dataController.load()
+            let navigationController = window?.rootViewController as! UINavigationController
+            let viewController = navigationController.topViewController as! TravelLocationsMapViewController
+            viewController.dataController = dataController
+            return
+        }
+    }
+    
+    func saveViewContext() {
+        // iOS 13 and above use SceneDelegate for this
+        guard #available(iOS 13.0, *) else {
+            try? dataController.viewContext.save()
+            return
+        }
     }
 
 }
